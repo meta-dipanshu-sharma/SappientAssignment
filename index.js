36f63efb7d60b74515b2b4323fb78d79990bea73
meta-dipanshu-sharma <41181764+meta-dipanshu-sharma@users.noreply.github.com>
2020-08-31 19:56:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+//import './index.css';
+import HomePage from './container/HomePage';
+
+ReactDOM.hydrate(
+  <React.StrictMode>
+    <Router>
+      <HomePage />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
