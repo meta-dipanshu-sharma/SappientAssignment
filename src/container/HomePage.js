5e74d9a8fc842c2e4d6ab90c1175104938b4a646
meta-dipanshu-sharma <41181764+meta-dipanshu-sharma@users.noreply.github.com>
2020-08-31 23:56:43 +0530

    Update HomePage.js

--- src/container/HomePage.js
+++ src/container/HomePage.js
@@ -1,54 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Datatable from '../component/data-table'
-import { withRouter } from 'react-router';
-import './style.scss'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Datatable from "../component/data-table";
+import { withRouter } from "react-router";
+import "./style.scss";
 
 const Homepage = (props) => {
-
-  const [url,] = useState('https://api.spaceXdata.com/v3/launches?')
-  const [pathParamUrl, setPathParamUrl] = useState('limit=100')
-  const [satelliteData, setSatelliteData] = useState()
-  const [launchSuccess, setLaunchSuccess] = useState()
-  const [landSuccess, setLandSuccess] = useState()
-  const [selectedLaunchYear, setSelectedLaunchYear] = useState()
+  const [url] = useState("https://api.spaceXdata.com/v3/launches?");
+  const [pathParamUrl, setPathParamUrl] = useState("limit=100");
+  const [satelliteData, setSatelliteData] = useState();
+  const [launchSuccess, setLaunchSuccess] = useState();
+  const [landSuccess, setLandSuccess] = useState();
+  const [selectedLaunchYear, setSelectedLaunchYear] = useState();
 
   useEffect(() => {
-    let pathParams = 'limit=100'
+    let pathParams = "limit=100";
 
-    pathParams = launchSuccess ? `${pathParams}&launch_success=${launchSuccess}` : `${pathParams}`
+    pathParams = launchSuccess
+      ? `${pathParams}&launch_success=${launchSuccess}`
+      : `${pathParams}`;
 
-    pathParams = landSuccess ? `${pathParams}&land_success=${landSuccess}` : `${pathParams}`
+    pathParams = landSuccess
+      ? `${pathParams}&land_success=${landSuccess}`
+      : `${pathParams}`;
 
-    pathParams = selectedLaunchYear ? `${pathParams}&launch_year=${selectedLaunchYear}` : `${pathParams}`
+    pathParams = selectedLaunchYear
+      ? `${pathParams}&launch_year=${selectedLaunchYear}`
+      : `${pathParams}`;
 
-    setPathParamUrl(pathParams)
+    setPathParamUrl(pathParams);
 
     const fetchData = async () => {
-      return axios
-        .get(`${url}${pathParamUrl}`)
-        .then(response => {
-          setSatelliteData(response.data)
-        })
-    }
+      return axios.get(`${url}${pathParamUrl}`).then((response) => {
+        setSatelliteData(response.data);
+      });
+    };
 
     props.history.push(`/api.spaceXdata.com/v3/launches?${pathParamUrl}`);
 
-    pathParams === pathParamUrl && fetchData()
+    pathParams === pathParamUrl && fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [launchSuccess, landSuccess, selectedLaunchYear, pathParamUrl])
+  }, [launchSuccess, landSuccess, selectedLaunchYear, pathParamUrl]);
 
   const handleLandFilter = (value) => {
-    landSuccess && landSuccess === value ? setLandSuccess() : setLandSuccess(value)
-  }
+    landSuccess && landSuccess === value
+      ? setLandSuccess()
+      : setLandSuccess(value);
+  };
 
   const handleLaunchFilter = (value) => {
-    launchSuccess && launchSuccess === value ? setLaunchSuccess() : setLaunchSuccess(value)
-  }
+    launchSuccess && launchSuccess === value
+      ? setLaunchSuccess()
+      : setLaunchSuccess(value);
+  };
 
   const handleYearFilter = (value) => {
-    selectedLaunchYear && selectedLaunchYear === value ? setSelectedLaunchYear() : setSelectedLaunchYear(value)
-  }
+    selectedLaunchYear && selectedLaunchYear === value
+      ? setSelectedLaunchYear()
+      : setSelectedLaunchYear(value);
+  };
 
   return (
     <>
@@ -62,13 +71,28 @@ const Homepage = (props) => {
         launchSuccess={launchSuccess}
         launchYear={launchYear}
         selectedLaunchYear={selectedLaunchYear}
-
       />
       <h3>Developed by: Dipanshu Sharma</h3>
     </>
-  )
-}
+  );
+};
 
-const launchYear = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+const launchYear = [
+  2006,
+  2007,
+  2008,
+  2009,
+  2010,
+  2011,
+  2012,
+  2013,
+  2014,
+  2015,
+  2016,
+  2017,
+  2018,
+  2019,
+  2020
+];
 
-export default withRouter(Homepage)
+export default withRouter(Homepage);
